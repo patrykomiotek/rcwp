1de5cf94adaf62595316b25fe45edf84aff409c1
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-26 14:08:08 +0200

    dispatching actions

--- src/components/User/UsersList.tsx
+++ src/components/User/UsersList.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
+import { Action, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import type { Store } from '../../store';
 import type { User } from './store';
 
 type UserListProps = {
-  users: User[]
+  users: User[];
+  fetchUsers: any;
+  fetchOneUser: any;
 }
 
-const UsersList = ({ users }: UserListProps) => {
+const UsersList = ({ users, fetchUsers, fetchOneUser }: UserListProps) => {
+  useEffect(() => {
+    fetchUsers();
+    const userid = 456;
+    fetchOneUser(userid);
+  }, []);
   return (
     <div>
       {users && users.map((elem) => (
@@ -21,8 +30,26 @@ const mapStateToProps = (state: Store) => ({
   users: state.users.data,
 });
 
+const mapDispatchToProps = (dispatch: any) => ({
+  fetchUsers: () => dispatch({
+    type: 'users/fetch',
+    payload: [{
+      id: "3",
+      name: "Ala"
+    }, {
+      id: "4",
+      name: "Jola"
+    }]
+  }),
+  fetchOneUser: (userId: number) => dispatch({
+    type: 'users/fetchOneUser',
+    payload: { userId }
+  })
+});
+
 const UserListConnected = connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps,
 )(UsersList);
 
 export {
