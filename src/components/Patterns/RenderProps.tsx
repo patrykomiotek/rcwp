f27f6138fe3ac4b1975aa89e91b9b25316f04ce8
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-10-27 12:31:45 +0200

    mouse and name provider with fragment

--- src/components/Patterns/RenderProps.tsx
+++ src/components/Patterns/RenderProps.tsx
@@ -1,29 +1,29 @@
+import { Fragment } from 'react';
 import { NameProvider } from './NameProvider';
 import { MouseProvider } from './MouseProvider';
 
 const NameProviderComponent = () => {
   return (
-    <div>
-      {/* <NameProvider /> */}
-      {/* <NameProvider></NameProvider> */}
-
+    <>
+      {/* <NameProvider /> 🐒 */}
+      {/* <NameProvider></NameProvider> 🐒 */}
       <NameProvider>
         {({ name }) => (<h1>Hello {name}!</h1>)}
       </NameProvider>
-    </div>
+    </>
   );
 }
 type MouseInfoProps = { x: number; y: number };
 const MouseInfo = ({ x, y }: MouseInfoProps) => <p>Mouse position is x={x}, y={y}</p>;
 const MouseProviderComponent = () => {
   return (
-    <div>
+    <Fragment>
       <h2>Mouse provider</h2>
       <MouseProvider
         render={(state) => (
           <MouseInfo x={state.x} y={state.y} />
         )} />
-    </div>
+    </Fragment>
   );
 }
 
